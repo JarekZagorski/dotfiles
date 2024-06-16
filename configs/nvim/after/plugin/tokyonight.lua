bafb8d170bfde37000856e8a7f049b2294a79284
Jarek Zagórski <me.kubolski@gmail.com>
2024-06-16 16:10:20 +0200

    nvim themes and tmux fixes

--- configs/nvim/after/plugin/tokyonight.lua
+++ configs/nvim/after/plugin/tokyonight.lua
@@ -1,20 +1,29 @@
 ---@diagnostic disable: missing-fields
 require("tokyonight").setup({
-	style = 'night',
+    style = 'night',
     ligt_style = 'day',
-	transparent = false,
-	styles = {
-		-- Style to be applied to different syntax groups
-		-- Value is any valid attr-list value for `:help nvim_set_hl`
-		comments = { italic = true },
-		keywords = { italic = true },
-		functions = {},
-		variables = {},
-		-- Background styles. Can be "dark", "transparent" or "normal"
-		sidebars = "dark", -- style for sidebars, see below
-		floats = "dark", -- style for floating windows
-	},
-	sidebars = { "qf", "help" },
+    transparent = false,
+    styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+    },
+    sidebars = { "qf", "help" },
 })
 
-vim.cmd.colorscheme('tokyonight')
+require('auto-dark-mode').setup({
+        update_interval = 5000,
+        set_dark_mode = function()
+            vim.cmd("colorscheme tokyonight-night")
+        end,
+        set_light_mode = function()
+            vim.cmd("colorscheme tokyonight-day")
+        end,
+    }
+)
--- configs/nvim/lua/custom/plugins/colorschemes.lua
+++ configs/nvim/lua/custom/plugins/colorschemes.lua
@@ -1,13 +1,16 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		'edkolev/tmuxline.vim',
-		lazy = false,
-		priority = 1000,
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
     },
+    {
+        'edkolev/tmuxline.vim',
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "f-person/auto-dark-mode.nvim",
+    }
 }
