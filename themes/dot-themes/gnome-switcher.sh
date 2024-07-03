af0ab6f80f0ef5b345a938019df871df38607771
Jarek Zagórski <me.kubolski@gmail.com>
2024-07-03 23:47:09 +0200

    fixed libadwaita theme changer

--- themes/dot-themes/gnome-switcher.sh
+++ themes/dot-themes/gnome-switcher.sh
@@ -7,14 +7,14 @@ if [[ $1 == dark ]]; then
 	dconf write /org/gnome/desktop/interface/icon-theme "'Tokyonight-Dark'"
 	dconf write /org/gnome/desktop/interface/gtk-theme "'Tokyonight-Dark'"
     echo "import = [ '~/.config/alacritty/alacritty-auto-theme/dark_theme.toml' ]" > /home/jakub/.config/alacritty/alacritty-auto-theme/theme.toml
-    /home/jakub/dotfiles/themes/libadwaita-theme-changer/libadwaita-tc.py --gsettings
+    /home/jakub/.themes/libadwaita-theme-changer/libadwaita-tc.py --gsettings
 	echo "Dark"
 else
 	dconf write /org/gnome/shell/extensions/user-theme/name "'Tokyonight-Light'"
 	dconf write /org/gnome/desktop/interface/icon-theme "'Tokyonight-Light'"
 	dconf write /org/gnome/desktop/interface/gtk-theme "'Tokyonight-Light'"
     echo "import = [ '~/.config/alacritty/alacritty-auto-theme/light_theme.toml' ]" > /home/jakub/.config/alacritty/alacritty-auto-theme/theme.toml
-    /home/jakub/dotfiles/themes/libadwaita-theme-changer/libadwaita-tc.py --gsettings
+    /home/jakub/.themes/libadwaita-theme-changer/libadwaita-tc.py --gsettings
 	echo "Light"
 fi
 
