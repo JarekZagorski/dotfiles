3c20405152c69f26e951e9cfab426e85c8e434ea
Jarek Zagórski <me.kubolski@gmail.com>
2024-10-13 01:00:25 +0200

    nvim - added statusline

---
+++ config/dot-config/nvim/after/plugin/lualine.lua
@@ -0,0 +1,49 @@
+local mode = require 'lualine.components.diff'
+
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 500,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    sections = {
+        lualine_a = { 'mode', {
+            function()
+                return string.format('Rec [%s]', vim.fn.reg_recording())
+            end,
+            cond = function()
+                return vim.fn.reg_recording() ~= ''
+            end,
+        } },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
+}
---
+++ config/dot-config/nvim/lua/plugins/looks.lua
@@ -0,0 +1,7 @@
+-- in there shall be plugins concerning looks and feels of nvim
+return {
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    }
+}
