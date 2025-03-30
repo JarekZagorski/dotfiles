b4ebae91a803e3e21a9e73c1f127710579077b8d
Jarek Zagórski <me.kubolski@gmail.com>
2025-03-30 11:09:35 +0200

    feat: adjust keymap

--- config/dot-config/nvim/lua/config/keymap.lua
+++ config/dot-config/nvim/lua/config/keymap.lua
@@ -10,17 +10,24 @@ vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'File Grep' })
 vim.keymap.set('n', '<leader>pv', '<cmd>Yazi toggle<cr>', { desc = 'Project View' })
 vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Yank line to clipboard' })
 
+---@param opts vim.diagnostic.JumpOpts
+local function diagnostic_jump(opts)
+    return function()
+        vim.diagnostic.jump(opts)
+    end
+end
+
 -- lsp keymaps
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Display Hover Information' })
 vim.keymap.set('n', 'gdd', vim.lsp.buf.definition, { desc = 'Jump to definition' })
-vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, { desc = 'Jump to type definition' })
+vim.keymap.set('n', 'gdt', vim.lsp.buf.type_definition, { desc = 'Jump to type definition' })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Jump to Declaration' })
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'List implementations' })
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'List all references' })
 vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { desc = 'Display signature information' })
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = 'Show Diagnostics' })
-vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { desc = 'Rename' })
-vim.keymap.set('n', '<F3>', vim.lsp.buf.format, { desc = 'Format buffer using attached lsp' })
-vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, { desc = 'Code action' })
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostics' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostics' })
+vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Code Rename' })
+vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Code Format buffer using attached lsp' })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
+vim.keymap.set('n', '[d', diagnostic_jump { count = -1, float = true }, { desc = 'Previous diagnostics' })
+vim.keymap.set('n', ']d', diagnostic_jump { count = 1, float = true }, { desc = 'Next diagnostics' })
