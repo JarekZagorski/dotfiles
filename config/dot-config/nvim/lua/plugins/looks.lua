6a6e2dcbd989ed526636ac3d83d20f802cab0e02
Jarek Zagórski <me.kubolski@gmail.com>
2025-04-15 21:06:04 +0200

    wip

--- config/dot-config/nvim/lua/plugins/looks.lua
+++ config/dot-config/nvim/lua/plugins/looks.lua
@@ -35,6 +35,10 @@ return {
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
-        }
+        },
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
 }
