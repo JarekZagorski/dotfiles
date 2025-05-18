30cd6d858c1b386c1347cdc765385f8732cc2a77
Jarek Zagórski <me.kubolski@gmail.com>
2025-05-18 11:56:58 +0200

    feat(emacs): add doom emacs config

---
+++ config/dot-config/doom/custom.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("794cf572e8bd9028f5e3565261e4f12e9ce537fd8f200331c1a34b587f05ef4b"
+     "ce32806f008842afce5406c7d0deed58e879354655835bfc24e4d8e27b652d8e"
+     "2865a6070f7e8b4b47b598fe15c7ec5b0e87f4378c9323d35f9f34ef354560ba"
+     "3769087436ae8a86e8a69928925a24ed038670a5473a75349a8fec76472745cd"
+     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
+ '(safe-local-variable-values '((eval when (fboundp 'rainbow-mode) (rainbow-mode 1)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
