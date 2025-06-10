d8d643e098a6fecf0848ae2262f6004254bd3b90
Jarek Zagórski <me.kubolski@gmail.com>
2025-06-10 23:48:27 +0200

    feat(fish): remove unused function

--- config/dot-config/fish/functions/l.fish
+++
@@ -1,3 +0,0 @@
-function l --wraps='ls -alh | less -r' --wraps='ls -alh --color=always | less -r' --description 'alias l=ls -alh --color=always | less -r'
-  ls -alh --color=always | command less -r $argv; 
-end
