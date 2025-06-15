9cc782a5cf403f5353f721069b4a881c6c7eefd2
Jarek Zagórski <me.kubolski@gmail.com>
2025-06-15 20:48:58 +0200

    refactor(nvim): cleanup in keymap

--- config/dot-config/nvim/lua/config/keymap.lua
+++ config/dot-config/nvim/lua/config/keymap.lua
@@ -1,21 +1,23 @@
+-- some useful functions
+
+local function a(f, opts)
+  return function()
+    f(opts)
+  end
+end
+
 -- Telescope
 local telescope = require('telescope.builtin')
 --vim.keymap.set('n', '<C-p>', telescope.git_files, {})
--- Fugitive
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Git Fugitive: git status' })
--- vim.keymap.set('n', '<leader>gc', vim.cmd.Git, { desc = 'Git Fugitive: git status' })
+
+-- Git
+vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { desc = 'Git Fugitive: git status' })
+
 -- <leader> segment
 vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Find File' })
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'File Grep' })
 vim.keymap.set('n', '<leader>pv', '<cmd>Yazi toggle<cr>', { desc = 'Project View' })
 vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Yank line to clipboard' })
-
----@param opts vim.diagnostic.JumpOpts
-local function diagnostic_jump(opts)
-  return function()
-    vim.diagnostic.jump(opts)
-  end
-end
 
 -- lsp keymaps
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Display Hover Information' })
@@ -26,13 +28,24 @@ vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'List implementat
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'List all references' })
 vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { desc = 'Display signature information' })
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = 'Show Diagnostics' })
-vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Code Rename' })
-vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Code Format buffer using attached lsp' })
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
-vim.keymap.set('n', '[d', diagnostic_jump { count = -1, float = true }, { desc = 'Previous diagnostics' })
-vim.keymap.set('n', ']d', diagnostic_jump { count = 1, float = true }, { desc = 'Next diagnostics' })
+vim.keymap.set('n', '[d', a(vim.diagnostic.jump, { count = -1, float = true }), { desc = 'Previous diagnostics' })
+vim.keymap.set('n', ']d', a(vim.diagnostic.jump, { count = 1, float = true }), { desc = 'Next diagnostics' })
 
--- harpoon
+--------------------
+--- code actions ---
+--------------------
+
+vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Code rename' })
+vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Code format buffer using attached lsp' })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
+vim.keymap.set('n', '<leader>co', a(vim.lsp.buf.code_action, {
+  filter = function(x) return x.kind == "source.organizeImports" end,
+  apply = true,
+}), { desc = 'Code action: Organize Imports' })
+
+---------------
+--- harpoon ---
+---------------
 local harpoon = require 'harpoon'
 
 -- required
@@ -45,3 +58,7 @@ vim.keymap.set('n', '<C-h>', function() harpoon:list():select(1) end)
 vim.keymap.set('n', '<C-j>', function() harpoon:list():select(2) end)
 vim.keymap.set('n', '<C-k>', function() harpoon:list():select(3) end)
 vim.keymap.set('n', '<C-l>', function() harpoon:list():select(4) end)
+
+---------------
+--- harpoon ---
+---------------
