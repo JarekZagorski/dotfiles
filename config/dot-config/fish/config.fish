29a7e1c4b3ddc0741b6b3ab8bd93f4f6dfdc932d
Jarek Zagórski <me.kubolski@gmail.com>
2025-08-15 15:25:07 +0200

    feat(fish): add hook for opam config

--- config/dot-config/fish/config.fish
+++ config/dot-config/fish/config.fish
@@ -11,6 +11,7 @@ alias vim "nvim"
 set TEMPL_EXPERIMENT rawgo 
 
 set LESS -r
+set EDITOR vim
 
 # from any-nix-shell
 if type -q any-nix-shell 
@@ -19,5 +20,12 @@ end
 
 set DOTNET_CLI_TELEMETRY_OPTOUT 'true'
 
-source "$HOME/.cargo/env.fish"  # For fish
+# source "$HOME/.cargo/env.fish"  # For fish
 
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/jakub/.opam/opam-init/init.fish' && source '/home/jakub/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
