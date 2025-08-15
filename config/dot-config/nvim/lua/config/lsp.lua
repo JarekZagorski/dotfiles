df51e3ab9cdf78f8aa3ff706242d5c34bfde8fad
Jarek Zagórski <me.kubolski@gmail.com>
2025-08-15 15:20:01 +0200

    feat(nvim): add auto format on save and enable more languages

--- config/dot-config/nvim/lua/config/lsp.lua
+++ config/dot-config/nvim/lua/config/lsp.lua
@@ -1,12 +1,25 @@
 vim.lsp.config('*', {
   -- this one adds clients capabilities
-  capabilities = require('cmp_nvim_lsp').default_capabilities()
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  ---@type vim.lsp.client.on_attach_cb
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('Format', { clear = true }),
+        buffer = bufnr,
+        callback = function() vim.lsp.buf.format({ bufnr = bufnr }) end,
+      })
+    end
+  end
 })
 
 local enabled_servers = {
   'lua_ls',
   'zls',
   'gopls',
+  'cssls',
+  'nushell',
+  'ocamllsp',
 }
 
 -- provide each server with some default config, else it fails
