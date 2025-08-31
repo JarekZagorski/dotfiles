7231972db90ab0aa212fb2f6118d849e34c52f87
Jarek Zagórski <me.kubolski@gmail.com>
2025-08-31 19:53:24 +0200

    patch: cleanup configs

--- config/dot-config/wezterm/wezterm.lua
+++ config/dot-config/wezterm/wezterm.lua
@@ -66,7 +66,7 @@ config.automatically_reload_config = true
 config.font = wezterm.font "Fira Code"
 config.font_size = 12.0 -- 11 on windows
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 config.color_scheme = get_scheme(get_appearance())
 config.background = get_background(get_appearance())
 
