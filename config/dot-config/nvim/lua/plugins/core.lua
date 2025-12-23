2bbb2a8a2d19720f9b058b3240b0ac0d9edd4b5d
Jarek Zagórski <me.kubolski@gmail.com>
2025-12-23 21:02:31 +0100

    feat(nvim): drop some looks only plugins and delete configs for them

--- config/dot-config/nvim/lua/plugins/core.lua
+++ config/dot-config/nvim/lua/plugins/core.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',     -- may remove this
+    tag = '0.1.8', -- may remove this
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
@@ -13,7 +13,7 @@ return {
   },
   {
     'folke/lazydev.nvim',
-    ft = 'lua',     -- only load on lua files
+    ft = 'lua', -- only load on lua files
     opts = {
       library = {
         -- See the configuration section for more details
