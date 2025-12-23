e128da739aef2a0f477b887c1e6cb82c88763dad
Jarek Zagórski <me.kubolski@gmail.com>
2025-12-23 21:02:59 +0100

    feat(nvim): adjust neovide settings

--- config/dot-config/nvim/lua/config/neovide.lua
+++ config/dot-config/nvim/lua/config/neovide.lua
@@ -1,5 +1,10 @@
-if vim.g.neovide then
-  vim.g.neovide_opacity = 0.5
-  vim.g.neovide_normal_opacity = 0.5
-  vim.g.neovide_cursor_animation_length = 0.05
+if not vim.g.neovide then
+  return
 end
+
+vim.g.neovide_opacity = 0.5
+vim.g.neovide_normal_opacity = 0.5
+
+-- animation settings
+vim.g.neovide_cursor_animation_length = 0.040
+vim.g.neovide_cursor_short_animation_length = 0.03
