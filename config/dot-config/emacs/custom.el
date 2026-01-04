432c18666e5e90705d251c3ead083966cdc88bf5
Jarek Zagórski <me.kubolski@gmail.com>
2026-01-04 18:11:53 +0100

    chore(emacs): update custom.el

--- config/dot-config/emacs/custom.el
+++ config/dot-config/emacs/custom.el
@@ -4,7 +4,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(custom-safe-themes
+   '("f9d423fcd4581f368b08c720f04d206ee80b37bfb314fa37e279f554b6f415e9"
+	 "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e"
+	 default))
+ '(package-selected-packages
+   '(catppuccin-theme company corfu evil-collection flycheck
+					  git-commit-ts-mode git-modes lsp-mode marginalia
+					  nerd-icons orderless stimmung-themes vertico
+					  zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
