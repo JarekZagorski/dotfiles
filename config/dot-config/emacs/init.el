d391c142a0a0ba9dd9297c4512943e3f4b4efc52
Jarek Zagórski <me.kubolski@gmail.com>
2026-01-06 21:31:55 +0100

    feat(emacs): add "TODO" highlight package

--- config/dot-config/emacs/init.el
+++ config/dot-config/emacs/init.el
@@ -143,6 +143,19 @@
   ;; but you can use any other Nerd Font if you want
   (nerd-icons-font-family "Fira Code"))
 
+(use-package hl-todo
+  :hook (prog-mode . hl-todo-mode)
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+		hl-todo-keyword-faces
+		`(("TODO"       warning bold)
+         ("FIXME"      error bold)
+         ("BUG"      error bold)
+         ("HACK"       font-lock-constant-face bold)
+         ("REVIEW"     font-lock-keyword-face bold)
+         ("NOTE"       success bold)
+         ("DEPRECATED" font-lock-doc-face bold))))
+
 ;; Download and Enable Evil
 (use-package evil
   :ensure t
