be9c9f9474a65eabc7a9a84fefe26ea09cb0fd4d
Jarek Zagórski <me.kubolski@gmail.com>
2026-01-06 21:31:28 +0100

    chore(emacs): update custom.el

--- config/dot-config/emacs/custom.el
+++ config/dot-config/emacs/custom.el
@@ -14,8 +14,9 @@
  '(lsp-ui-doc-show-with-mouse nil)
  '(package-selected-packages
    '(catppuccin-theme corfu evil-collection flycheck git-commit-ts-mode
-					  git-modes lsp-mode lsp-ui marginalia nerd-icons
-					  orderless stimmung-themes vertico zig-mode)))
+					  git-modes hl-todo lsp-mode lsp-ui marginalia
+					  nerd-icons orderless stimmung-themes vertico
+					  zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
